02391847b737963cbf42f1e843f204929c52a46f
Sara-Castaneda25 <saracastanedaecheverri@gmail.com>
2024-01-24 16:53:01 -0500

    AÑADIENDO

--- app/index.js
+++ app/index.js
@@ -1,9 +1,19 @@
 //express me permite levantar el servidor
 import  express  from "express";
 
-// server
+// fix para dirname
+import path from 'path';
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// server
 const app = express();
 app.set("port", 4000);
 app.listen(app.get("port"));
 console.log("servidor corriendo en el puerto", app.get("port"));
+
+//config
+app.use(express.static(__dirname + "/public"));
+
+// rutas
+app.get("/", (req,res)=> res.sendFile(__dirname + "/pages/login.html"))
